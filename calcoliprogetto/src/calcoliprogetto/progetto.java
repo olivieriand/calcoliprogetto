0fb9fe64d83e7e8578beb5ee9ed3fd9b88fb1c0d
andrea olivieri <andre.olly2005@gmail.com>
2023-11-13 11:33:39 +0100

    2.definizione attributi classe progetto

--- calcoliprogetto/src/calcoliprogetto/progetto.java
+++ calcoliprogetto/src/calcoliprogetto/progetto.java
@@ -2,5 +2,21 @@ package calcoliprogetto;
 
 public class progetto
 {
+	private int BAC;
+	private int AttPiani;
+	private int AttAttuali;
+	private int CostoPiani;
+	private int CostoAtt;
+	public progetto(int bAC, int attPiani, int attAttuali, int costoPiani, int costoAtt) {
+		super();
+		BAC = bAC;
+		AttPiani = attPiani;
+		AttAttuali = attAttuali;
+		CostoPiani = costoPiani;
+		CostoAtt = costoAtt;
+	}
+	
+	
+	
 
 }
